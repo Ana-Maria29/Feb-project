cbb8ff5f85389bffd14314e2985042684bbdc38b
Ana-Maria29 <anamaria.apostoaiei@gmail.com>
2017-02-19 18:44:52 +0100

    Angular

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: "prod-description",
+	template: `
+	<p>Sportswear is typically designed to be lightweight so as not to encumber the wearer. The best athletic wear for some forms of exercise, for example cycling, should not create drag or be too bulky.On the other hand, sportswear should be loose enough so as not to restrict movement.</p>
+	`
+})
+export class AppComponent { }
---
+++ dist/app.component.js
@@ -0,0 +1,21 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: "prod-description",
+        template: "\n\t<p>Sportswear is typically designed to be lightweight so as not to encumber the wearer. The best athletic wear for some forms of exercise, for example cycling, should not create drag or be too bulky.On the other hand, sportswear should be loose enough so as not to restrict movement.</p>\n\t"
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
